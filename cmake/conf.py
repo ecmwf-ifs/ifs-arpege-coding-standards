008d0cc8953320abe3c1a3c8a96b81c72e947e00
Michael Sleigh <michael.sleigh@ecmwf.int>
2025-12-01 15:59:16 +0000

    Update conf.py in cmake (correct theme etc.)

--- cmake/conf.py
+++ cmake/conf.py
@@ -1,29 +1,52 @@
+# -*- coding: utf-8 -*-
+#
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'IFS CMake Standard'
-copyright = '2025, ECMWF'
+copyright = '2025-, ECMWF'
 author = 'ECMWF'
+
+# The short X.Y version
+version = '0.1'
+# The full version, including alpha/beta/rc tags
 release = '0.1'
 
+
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = []
+
+# The suffix(es) of source filenames.
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+language = 'en'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 root_doc = 'index'
 
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    'collapse_navigation': True,
+}
+
 html_static_path = ['_static']
+
